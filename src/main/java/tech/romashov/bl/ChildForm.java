434ab173b45b9ae3b7ce69ed84f3a181ead8b742
Dmitry Romashov <framebassman@gmail.com>
2021-01-25 09:47:34 +0300

    fix for hidden elements

--- src/main/java/tech/romashov/bl/ChildForm.java
+++ src/main/java/tech/romashov/bl/ChildForm.java
@@ -2,33 +2,56 @@ package tech.romashov.bl;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideDriver;
+import com.codeborne.selenide.SelenideElement;
 import org.slf4j.Logger;
 import tech.romashov.ApplicationProperties;
 
+import static com.codeborne.selenide.Selectors.byText;
+
 public class ChildForm extends AbstractForm implements IFillable {
+    private int index = 0;
+
     public ChildForm(Logger log, SelenideDriver selenide, ApplicationProperties props, ElementsCollection elements) {
         super(log, selenide, props, elements);
     }
 
     @Override
     public void fill() {
-        wrappers.get(0).$("input").clear();
-        wrappers.get(0).$("input").setValue(properties.getProperty("child_snils"));
+        SelenideElement snils = next();
+        snils.$("input").clear();
+        snils.$("input").setValue(properties.getProperty("child_snils"));
 
-        wrappers.get(1).$("input").clear();
-        wrappers.get(1).$("input").setValue(properties.getProperty("child_surname"));
+        SelenideElement child_surname = next();
+        child_surname.$("input").clear();
+        child_surname.$("input").setValue(properties.getProperty("child_surname"));
 
-        wrappers.get(2).$("input").clear();
-        wrappers.get(2).$("input").setValue(properties.getProperty("child_name"));
+        SelenideElement child_name = next();
+        child_name.$("input").clear();
+        child_name.$("input").setValue(properties.getProperty("child_name"));
 
-        wrappers.get(3).$("input").clear();
-        wrappers.get(3).$("input").setValue(properties.getProperty("child_middlename"));
+        SelenideElement child_middlename = next();
+        child_middlename.$("input").clear();
+        child_middlename.$("input").setValue(properties.getProperty("child_middlename"));
 
-//        wrappers.get(4).$("input").clear();
-//        wrappers.get(4).$("input").setValue(properties.getProperty("building"));
+        SelenideElement sex = next();
+//        sex.$(byText("Женский")).click();
 
-        wrappers.get(5).$("input").clear();
-        wrappers.get(5).$("input").setValue(properties.getProperty("date_of_birth"));
+        SelenideElement date_of_birth = next();
+        date_of_birth.$("input").clear();
+        date_of_birth.$("input").setValue(properties.getProperty("date_of_birth"));
+    }
+
+    private SelenideElement next() {
+        try {
+            while (!wrappers.get(index).isDisplayed()) {
+                index++;
+            }
+
+            return wrappers.get(index);
+        }
+        finally {
+            index++;
+        }
     }
 
     @Override
